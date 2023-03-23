259bc46b2837cc72ba793b75996adfbe9a375469
visantiago175 <santivides01@gmail.com>
2023-03-22 19:08:16 -0500

    commit cero

---
+++ src/test/java/com/canchas_admin/administrador/AdministradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canchas_admin.administrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
